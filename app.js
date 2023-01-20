cdbb42ad430c191fa609dc0c847909c947560574
nikhil-solankhi <nikhil.solankhi.ns@gmail.com>
2023-01-20 12:03:52 +0530

    sdm

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path=require("path");
+
+app.get("/", function(req,resp){
+    resp.send("welcome people")
+})
+
+app.get("/home",function(req,resp){
+    resp.sendFile(path.join(__dirname,"./public/resume.html"));
+})
+
+app.listen(8000);
+console.log("hello aliens !")
